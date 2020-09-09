8a132fc70295b6c9858cae5d2fd4b1787a849a60
Roma004 <klimov.roma04@yandex.ru>
2020-09-09 23:49:21 +0300

    example_config added

---
+++ config/example_conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Config:
+
+    MYSQL: dict = {
+        "db_name": "",
+        "user_name": "",
+        "password": "",
+        "host": "localhost"
+    }
+
+    VK_CONFIRMATION: str = ""
+    VK_SECRET: str = ""
+    VK_TOKEN: str = ""
